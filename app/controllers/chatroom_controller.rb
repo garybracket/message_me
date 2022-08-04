10497ab60836d65df10cb9bd637f0a0c445e8564
Gary McQueen <garybracket@hotmail.com>
2022-08-04 13:14:59 -0400

    add login and root routes

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,24 @@
+class ChatroomController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
