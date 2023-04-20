362e3794620219bb9234e80710d2dd0e063acb9c
unknown <Tiffy@tiffany.corp.no-illusion.com>
2023-04-19 23:28:49 -0400

    fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: "gary", password: "password")
-User.create(username: "tiff", password: "password")
-User.create(username: "haywee", password: "password")
+User.create(username: "gary", password: "password") if Rails.env.development?
+User.create(username: "tiff", password: "password") if Rails.env.development?
+User.create(username: "haywee", password: "password") if Rails.env.development?
